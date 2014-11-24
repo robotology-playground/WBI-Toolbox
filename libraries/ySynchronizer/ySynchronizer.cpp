a014f11d0f102c7595e9d72026164820e214feed
Jorhabib Eljaik <jorhabib@gmail.com>
2014-11-24 17:53:11 +0100

    New synchronization method

--- libraries/ySynchronizer/ySynchronizer.cpp
+++ libraries/ySynchronizer/ySynchronizer.cpp
@@ -17,6 +17,9 @@
 #include <time.h>
 #include <yarp/os/Time.h>
 #include <yarp/os/Network.h>
+#include <yarp/os/BufferedPort.h>
+#include <thrift/ClockServer.h>
+#include <thrift/ClockServer.h>
 
 static void mdlInitializeSizes(SimStruct *S)
 {
@@ -28,21 +31,27 @@ static void mdlInitializeSizes(SimStruct *S)
     ssSetNumContStates(S, 0);
     ssSetNumDiscStates(S, 0);
 
-    if (!ssSetNumInputPorts(S, 0)) return;
+//     if (!ssSetNumInputPorts(S, 0)) return;
 
-    if (!ssSetNumOutputPorts(S, 2)) return;
-    ssSetOutputPortWidth(S, 0, 1);	    		// Robot joint angular positions in radians
-    ssSetOutputPortWidth(S, 1, 1);
-    ssSetOutputPortDataType(S, 0, 0);
-    ssSetOutputPortDataType(S, 1, 0);
+//     if (!ssSetNumOutputPorts(S, 2)) return;
+//     ssSetOutputPortWidth(S, 0, 1);	    		// Robot joint angular positions in radians
+//     ssSetOutputPortWidth(S, 1, 1);
+//     ssSetOutputPortDataType(S, 0, 0);
+//     ssSetOutputPortDataType(S, 1, 0);
 
     ssSetNumSampleTimes(S, 1);
-    ssSetNumRWork(S, 3);
-    ssSetNumIWork(S, 0);
-    ssSetNumPWork(S, 0);
-    ssSetNumModes(S, 0);
-    ssSetNumNonsampledZCs(S, 0);
+    ssSetNumPWork(S, 2);
     ssSetOptions(S, 0);
+
+    ssSetOptions (S,
+                  SS_OPTION_WORKS_WITH_CODE_REUSE        |
+                  SS_OPTION_EXCEPTION_FREE_CODE          |   //we must be sure that every function we call never throws an exception
+                  SS_OPTION_ALLOW_INPUT_SCALAR_EXPANSION |
+                  SS_OPTION_USE_TLC_WITH_ACCELERATOR     |
+                  SS_OPTION_CALL_TERMINATE_ON_EXIT);
+
+    fprintf (stderr, "[mdlInitializeSizes] : Options set\n\n");
+
 }
 
 #define MDL_INITIALIZE_SAMPLE_TIMES
@@ -63,61 +72,56 @@ static void mdlStart(SimStruct *S)
         return;
     }
 
-    ssSetRWorkValue(S,0,ssGetTStart(S));         // Get initial simulation time.
-    ssSetRWorkValue(S,1,yarp::os::Time::now());  // Initial real time
-    ssSetRWorkValue(S,2,-1);                     // Real initial time of Matlab step.
-    yarp::os::Time::useNetworkClock("/clock");
+//  During startup I need this block to pause the simulation, then execute last
+//  and step the simulation by Ts.
+    yarp::os::Port *clientPort;
+    clientPort = new yarp::os::Port();
+
+    gazebo::ClockServer *clockServer;
+    clockServer = new gazebo::ClockServer();
+
+    clientPort->open("/ySynchronizer/clock:o");
+    if(!yarp::os::Network::connect("/ySynchronizer/clock:o","/clock/rpc"))
+        fprintf(stderr,"mdlStart : ERR Problems connecting with /clock/rpc. Has gazebo been launched with the clock plugin?\n");
+
+    clockServer->yarp().attachAsClient(*clientPort);
+
+    double stepSize = clockServer->getStepSize();
+    fprintf(stderr,"[mdlStart] : clockServer client retrieved stepSize = %lf", stepSize);
+
+    clockServer->pauseSimulation();
+
+    ssGetPWork(S) [0] = clientPort;
+    ssGetPWork(S) [1] = clockServer;
 }
 
 static void mdlOutputs(SimStruct *S, int_T tid)
 {
-    // Store real time at the beginning of each simulation step
-    double             t_c = yarp::os::Time::now();	                // Real time in current simulation step
-    real_T             t_system;
-    t_system = (real_T)clock()/CLOCKS_PER_SEC;
-//     time_T             t_SimTime = ssGetT(S);				// Useful to retrieve the simulation rate
-//     real_T             t_previousSimTime = ssGetRWorkValue(S,0);		// Useful to retrieve the simulation rate
-    
-    const real_T      *sample_time = mxGetPr(RATE(S));
-    
-//     Retrieve actual sampling time. For now assume the desired time scale factor is 1, i.e. Time ratio is 1:1
-//     real_T             fund_sample_time = t_SimTime - t_previousSimTime;  // 
-    
-    double             t_realSamplingTime = sample_time[0];
-
-    double             t_prev = static_cast<double>(ssGetRWorkValue(S,2));
-    
-    // if it's the first simulation step ...
-    if(t_prev<0) {
-	// ... then wait for the desired period ... 
-	yarp::os::Time::delay(static_cast<double>(sample_time[0]));  // First delay before execution of the other blocks
-    }
-    else {
-        // ... If it's not the first simulation step, then delay the simulation for 2Ts - (tc - tprev)
-        yarp::os::Time::delay(2*t_realSamplingTime - (t_c - t_prev));
-    }
-    
-    // and store the real time at the beginning of this simulation step ...
-    ssSetRWorkValue(S,2,t_c);
-    
-    // store also the matlab time at the beginning of this simulation step
-//     ssSetRWorkValue(S, 0, t_SimTime);
-    
-    real_T *pY0 = (real_T*)ssGetOutputPortSignal(S,0);
-    for(int_T j=0; j<ssGetOutputPortWidth(S,0); j++)
-//     pY0[j] = yarp::os::Time::now() - ssGetRWorkValue(S,1);
-        pY0[j] = t_c - ssGetRWorkValue(S,1);
-	
-    real_T *pY1 = (real_T*)ssGetOutputPortSignal(S,1);
-    for(int_T j=0; j<ssGetOutputPortWidth(S,1); j++)
-      pY1[j] = t_system;
-
+    gazebo::ClockServer *clockServer = static_cast<gazebo::ClockServer*> (ssGetPWork(S)[1]);
+    clockServer->stepSimulation(10000);
 }
 
 static void mdlTerminate(SimStruct *S)
 {
-    UNUSED_ARG(S); /* unused input argument */
-// I should use yarp::os::Network::fini() to terminate network connection but it currently hangs. Waiting for bug fix.
+//     UNUSED_ARG(S); /* unused input argument */
+    if (ssGetNumPWork(S) >0 && ssGetPWork (S)) {
+        fprintf(stderr,"Enteder mdlTerminate\n");
+        yarp::os::Port *clientPort = static_cast<yarp::os::Port*> (ssGetPWork(S)[0]);
+        if(!clientPort) {
+            clientPort->interrupt();
+            yarp::os::Network::disconnect("/ySynchronizer/clock:o","/clock/rpc");
+            clientPort->close();
+        }
+        gazebo::ClockServer *clockServer = static_cast<gazebo::ClockServer*> (ssGetPWork(S)[1]);
+        if(!clockServer)
+            delete clockServer;
+
+        if(!clientPort) {
+            delete clientPort;
+        }
+    }
+    yarp::os::Network::fini();
+    fprintf(stderr,"mdlTerminate : Reached end of this simulation\n");
 }
 
 /*
