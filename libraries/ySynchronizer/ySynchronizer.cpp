09d455889b1a3d50fafccff9ac907ba073e975f6
Jorhabib Eljaik <jorhabib@gmail.com>
2014-12-16 17:54:40 +0100

    Working and tested version of the ySynchronizer. Tests were carried out with the wholeBodyImpedance controller using the iCub model in Gazebo

--- libraries/ySynchronizer/ySynchronizer.cpp
+++ libraries/ySynchronizer/ySynchronizer.cpp
@@ -26,10 +26,10 @@ struct InternalData
     struct {
         std::string clientPortName;
         std::string serverPortName;
-        
+
         unsigned numberOfSteps;
     } configuration;
-    
+
     yarp::os::Port clientPort;
     gazebo::ClockServer clockServer;
 };
@@ -37,35 +37,30 @@ typedef struct InternalData InternalData;
 
 static void mdlInitializeSizes(SimStruct *S)
 {
-    
+
     ssSetNumSFcnParams(S, 1);  /* 1 Input parameters: Time Scale Factor, e.g. 1 for real time simulation */
-    
+
     if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) return;
-    
+
     ssSetNumContStates(S, 0);
     ssSetNumDiscStates(S, 0);
-    
-    //     if (!ssSetNumInputPorts(S, 0)) return;
-    
-    //     if (!ssSetNumOutputPorts(S, 2)) return;
-    //     ssSetOutputPortWidth(S, 0, 1);	    		// Robot joint angular positions in radians
-    //     ssSetOutputPortWidth(S, 1, 1);
-    //     ssSetOutputPortDataType(S, 0, 0);
-    //     ssSetOutputPortDataType(S, 1, 0);
-    
     ssSetNumSampleTimes(S, 1);
     ssSetNumPWork(S, 1);
+    ssSetNumDWork (S, 1);
+    ssSetDWorkWidth (S, 0, 1);
+    ssSetDWorkDataType (S, 0, SS_INTEGER);
     ssSetOptions(S, 0);
-    
+
     ssSetOptions (S,
                   SS_OPTION_WORKS_WITH_CODE_REUSE        |
                   SS_OPTION_EXCEPTION_FREE_CODE          |   //we must be sure that every function we call never throws an exception
                   SS_OPTION_ALLOW_INPUT_SCALAR_EXPANSION |
                   SS_OPTION_USE_TLC_WITH_ACCELERATOR     |
+                  SS_OPTION_PLACE_ASAP                   |   // This options makes the ySynchronizer execute AS SOON AS POSSIBLE
                   SS_OPTION_CALL_TERMINATE_ON_EXIT);
-    
+
     fprintf (stderr, "[mdlInitializeSizes] : Options set\n\n");
-    
+
 }
 
 #define MDL_INITIALIZE_SAMPLE_TIMES
@@ -78,80 +73,87 @@ static void mdlInitializeSampleTimes(SimStruct *S)
 #define MDL_START
 static void mdlStart(SimStruct *S)
 {
+    int_T* flag = (int_T*) ssGetDWork (S, 0);
+    flag[0] = 0;
+
     yarp::os::Network::init();
     printf("YARP Network initialized\n");
-    
+
     if (!yarp::os::Network::checkNetwork() || !yarp::os::Network::initialized()) {
         ssSetErrorStatus(S,"mdlStart >> YARP server wasn't found active!! \n");
         return;
     }
-    
+
     //  During startup I need this block to pause the simulation, then execute last
     //  and step the simulation by Ts.
     InternalData *internalData = new InternalData();
-    
-//    yarp::os::Port *clientPort;
-//    clientPort = new yarp::os::Port();
-//    
-//    gazebo::ClockServer *clockServer;
-//    clockServer = new gazebo::ClockServer();
-    
+
     internalData->configuration.clientPortName = "/ySynchronizer/clock:o"; //compose this with the module name?
-    
     internalData->configuration.serverPortName = "/clock/rpc";
-//    if (exists serverPortParameter)
-//        internalData->configuration.serverPortName = readParameter();
-    
+
     bool resetTimeAtStart = false;
-//    if (exists resetTimeAtStart parameter)
-//        resetTimeAtStart = read boolean variable;
-    
+
     double period = 0.01;
-    //read period from parameter here
-    
+    //TODO read period from parameter here
+
     internalData->clientPort.open(internalData->configuration.clientPortName);
     if (!yarp::os::Network::connect(internalData->configuration.clientPortName, internalData->configuration.serverPortName)) {
         fprintf(stderr,"mdlStart : ERR Problems connecting with /clock/rpc. Has gazebo been launched with the clock plugin?\n");
+        ssSetErrorStatus(S, "Problems connecting with /clock/rpc. Has gazebo been launched with the clock plugin?");
     }
-    
+
     internalData->clockServer.yarp().attachAsClient(internalData->clientPort);
-    
+
     double stepSize = internalData->clockServer.getStepSize();
-    fprintf(stderr,"[mdlStart] : clockServer client retrieved stepSize = %lf", stepSize);
-    
+    fprintf(stderr,"[ySynchronizer::mdlStart] >> clockServer client retrieved stepSize = %lf\n", stepSize);
+
     //check if stepSize / period is an integer.
     internalData->configuration.numberOfSteps = period / stepSize;
-    
-    internalData->clockServer.pauseSimulation();
-    
+
+//     internalData->clockServer.pauseSimulation();
+
     if (resetTimeAtStart) {
         internalData->clockServer.resetSimulationTime();
     }
-    
+
     ssGetPWork(S)[0] = internalData;
+
+    // Initialization is over.
+    flag[0]     = 1;
+
+    fprintf(stderr,"ySynchronizer::mdlStart >> Finished startup\n");
 }
 
 static void mdlOutputs(SimStruct *S, int_T tid)
 {
     InternalData *internalData = static_cast<InternalData*>(ssGetPWork(S)[0]);
+
+    int_T* flag = (int_T*) ssGetDWork (S, 0);
+    if (flag[0]) {
+        // Pause only once in mdlOutputs
+        internalData->clockServer.pauseSimulation();
+        flag[0] = 0;
+    }
     internalData->clockServer.stepSimulationAndWait(internalData->configuration.numberOfSteps);
 }
 
 static void mdlTerminate(SimStruct *S)
 {
-    //     UNUSED_ARG(S); /* unused input argument */
     if (ssGetNumPWork(S) > 0 && ssGetPWork(S)) {
         InternalData *internalData = static_cast<InternalData*>(ssGetPWork(S)[0]);
         if (internalData) {
-            yarp::os::Network::disconnect(internalData->configuration.clientPortName, internalData->configuration.serverPortName);
+            fprintf(stderr,"mdlTerminate >> Before issuing continueSimulation command\n");
+            internalData->clockServer.continueSimulation();
+            if(!yarp::os::Network::disconnect(internalData->configuration.clientPortName, internalData->configuration.serverPortName)) {
+                fprintf(stderr,"mdlTerminate >> ySynchronizer: Could not disconnect src %s from dest %s \n", internalData->configuration.clientPortName.c_str(), internalData->configuration.serverPortName.c_str());
+            }
             internalData->clientPort.close();
-            
             delete internalData;
             internalData = 0;
         }
     }
     yarp::os::Network::fini();
-    fprintf(stderr,"mdlTerminate : Reached end of this simulation\n");
+    fprintf(stderr,"mdlTerminate : ySynchronizer reached end of this simulation\n");
 }
 
 /*
